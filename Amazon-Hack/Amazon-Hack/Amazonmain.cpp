43b29d97684bf2c75c6fd94ce4bbb4400d92aeef
Ramachandran <ramachandranp@vembu.com>
2015-09-19 12:37:21 +0530

    Committed for mail file for testing purpose.

---
+++ Amazon-Hack/Amazon-Hack/Amazonmain.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"####### This much coding done for Amazon hach ####### \n";
+
+	int l_option;
+	cout<<" ########################## SELECTION MENU ######################## \n";
+	cout<<" ######\t 1. Shard Drive configuration \t########################### \n";
+	cout<<" ######\t 2. Parse CSV file \t\t########################### \n";
+	cout<<" ######\t 3. Search attribute \t\t########################### \n";
+	cout<<" ############################## END ############################### \n";
+	cout<<" Enter the option to perform :: \t ";
+	cin>>l_option;
+	switch(l_option)
+	{
+	case 1:
+		break;
+	default:
+		cout<<" Please enter the valid option to perform the task. Thanks!!!! \n";
+	}
+	cout<<" ########## The hack module ended up here ####### \n";
+	system("pause");
+	return 0;
+}
